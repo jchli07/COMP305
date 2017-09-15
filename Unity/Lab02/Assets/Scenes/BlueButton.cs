742d741f65246afce88fca872373cca5b30eb62b
jchli07 <james.ch.li07@gmail.com>
2017-09-14 22:19:56 -0400

    Lab02
    Created sprite with color shirts and buttons

---
+++ Unity/Lab02/Assets/Scenes/BlueButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueButton : MonoBehaviour {
+
+    public Sprite blueS;
+    public GameObject person;
+    SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+
+        rend = person.GetComponent<SpriteRenderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        rend.sprite = blueS;
+    }
+}
---
+++ Unity/Lab02/Assets/Scenes/RedButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedButton : MonoBehaviour {
+
+    public Sprite redS;
+    public GameObject person;
+    SpriteRenderer rend;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rend = person.GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        rend.sprite = redS;
+    }
+}
