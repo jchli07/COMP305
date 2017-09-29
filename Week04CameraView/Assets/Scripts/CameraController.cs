07e65cb5b264d54dcdcdb7d7dfac659a3fd8b8ce
jchli07 <james.ch.li07@gmail.com>
2017-09-28 21:34:01 -0400

    lab3
    Lab03

---
+++ Week04CameraView/Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform player;
+    private Transform camTrans;
+
+    // Use this for initialization
+    void Start()
+    {
+        camTrans = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (player.position.x < -3.5)
+        {
+            camTrans.position = new Vector3(player.position.x, player.position.y, -5);
+        }
+
+        else if (player.position.x > -3.5 && player.position.x < 4.9)
+        {
+            camTrans.position = new Vector3(0.94f, 0.32f, -5);
+        }
+
+        else if (player.position.x > 4.9)
+        {
+            camTrans.position = new Vector3(player.position.x, player.position.y, -5);
+
+            if (player.gameObject.GetComponent<Rigidbody2D>().velocity.x > 0)
+            {
+                Camera.main.orthographicSize -= 0.01f;
+            }
+
+            if (player.gameObject.GetComponent<Rigidbody2D>().velocity.x < 0)
+            {
+                Camera.main.orthographicSize += 0.01f;
+            }
+        }
+
+
+    }
+}
---
+++ Week04CameraView/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rBody;
+
+    // Use this for initialization
+    void Start()
+    { 
+        rBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        float horizMove = Input.GetAxis("Horizontal");
+        float vertMove = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizMove, vertMove, 0);
+        rBody.velocity = movement * speed;
+    }
+}
