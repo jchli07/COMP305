dcdda581d7948594d72afee1a01f67cadb7aaa19
jchli07 <james.ch.li07@gmail.com>
2017-10-04 22:58:57 -0400

    Lab04
    Lab04

---
+++ Lab04/Assets/Scripts/BombController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public float timeToDestroy = 1.0f;
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, timeToDestroy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+}
---
+++ Lab04/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+    public GameObject Bomb;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    
+
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print(Input.mousePosition);
+            Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+            print(p);
+            print(p.x);
+            print(p.y);
+
+            Instantiate(Bomb, new Vector3(p.x, p.y, 0.0f), Quaternion.identity);
+        }
+    }
+}
